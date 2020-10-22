6009ea994091270e935dafc2dad01bbc196e31c9
Kaxyubok <805526932@qq.com>
2020-10-22 16:29:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,6 @@
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-git clone https://github.com/Lienol/openwrt-package
+git clone https://github.com/destan19/OpenAppFilter.git
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
